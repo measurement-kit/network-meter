92468ff699d6f4ca5bc33eedca32eb0d568a4c46
poly <poly@darkdepths.net>
2015-06-23 19:13:51 +0400

    added plugin installation routine

--- plugin.js
+++ plugin.js
@@ -2,26 +2,30 @@ var exec = require('child_process').execFile;
 var remote = require('remote'); 
 var dialog = remote.require('dialog'); 
 var fs = require("fs");
+var path = require("path");
+var AdmZip = require("adm-zip");
+var events = require("events");
 
 var fileName = null;
 
 document.getElementById("plugin_select").onclick = function(){
-    fileName = dialog.showOpenDialog({ properties: [ 'openFile']}).toString();
-    if (fileName)
-        parsePlugin(fileName);
-}
-
-var parsePlugin = function(plugin){
-    fs.readFile(plugin, 'utf8', function (err,data) {
-        if (err) {
-            return console.log(err);
-        }
-        var pluginContents = data.toString();
-        console.log(pluginContents);
-        var jsonData = JSON.parse(pluginContents);
-
-        // just a temporary test
-        var temp = JSON.stringify(jsonData, null, 4);
-        document.getElementById("plugin_metadata").value = temp;
+    dialog.showOpenDialog({ properties: [ 'openFile'],
+    filters: [{name: 'Network Meter Plugins', extensions: ['nmp']}]}, function(data, err){
+        var fileName = data.toString();
+        fs.access(fileName, fs.R_OK, function(err) {
+            if (err) {
+                dialog.showErrorBox("File Error", "File you specified could not be accessed.");
+                return console.log(err);
+            }
+        });
+        installPlugin(fileName);
     });
+};
+
+var installPlugin = function(pluginPath){
+    var filename = path.basename(pluginPath, '.nmp');
+    var installFolder = path.resolve('plugins') + path.sep + filename + path.sep;
+
+    var zip = new AdmZip(pluginPath);
+    zip.extractAllTo(installFolder);
 };
