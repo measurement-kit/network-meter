e5ee48de92654b324f5eb18cf07c0b194ba3b71c
poly <poly@darkdepths.net>
2015-08-09 16:35:38 +0400

    command parsing functional

--- exec.js
+++ exec.js
@@ -12,27 +12,73 @@ var run = function() {
     });
 };
 
-exports.bindButtons = function () {
-    document.getElementById("ooni_button").onclick = function(){
-        args = document.getElementById("args").value;
-        run();
-    }
-
-    document.getElementById("file_button").onclick = function(){
-        fileName = dialog.showOpenDialog({ properties: [ 'openFile']});
-    }
-}
-
 exports.bindPluginGrid = function(state){
     list = document.getElementsByClassName("plugin");
     for (var i = 0; i < list.length; i++) { // save us ES6!
         list[i].addEventListener('click', function(object){
             state.run.set("selected", object.currentTarget.id);
             toggleOverlay();
+            bindRunButton(state);
         }, true);
     }
 }
 
+var bindRunButton = function(state) {
+    document.getElementById("run-button").addEventListener("click", 
+        function() { collectArgs(state) });
+};
+
+/* Reads arguments from the modal and generates a command
+ * to be run. Uses the template from plugins[x].command
+ * and replaces flags as appropriate */
+var collectArgs = function(state) {
+    var containers = document.getElementsByClassName("argument-container");
+    var args = [];
+    var selectedIndex = state.run.get("selected");
+
+    for(var i = 0; i < containers.length; i++) {
+        // activation refers to a checkbox that signifies if the user has activated
+        // the argument (only for optional arguments)
+        var activation = containers[i].getElementsByClassName("activation")[0];
+        if (activation && activation.checked || !activation) {
+            var option = containers[i].getElementsByTagName("input");
+            //relevant input is always the last one
+            option = option[option.length - 1];
+
+            var optionName = option.name.replace("arg-", "");
+            var optionValue;
+            
+            // if the arg has a flag, prepend the flag
+            var flagArray = state.run.get("plugins[" + selectedIndex + "].args");
+            var flagIndex;
+            for(var z = 0; z < flagArray.length; z++) {
+                if (flagArray[z].name == optionName) {
+                    flagIndex = z;
+                    break;
+                }
+            }
+            var flag = flagArray[flagIndex].flag;
+            optionValue = flag ? flag + " " + option.value : option.value;
+
+            args.push([optionName, optionValue]);
+        }
+    }
+
+    var command = state.run.get("plugins[" + selectedIndex + "].command");
+
+    for(var x = 0; x < args.length; x++) {
+        // args is an array of (flag name, flag value) tuples
+        command = command.replace("$" + args[x][0], args[x][1]);
+    }
+
+    // gets rid of any variables that have not been specified
+    command = command.replace(new RegExp(/\$[a-z][a-z0-9]*\s/g), "");
+
+    dialog.showMessageBox({"type": "info", "message": command, "buttons": ["close"]});
+};
+
+/* Toggles an overlay display with arguments for the user
+ * to enter */
 var toggleOverlay = function() {
     el = document.getElementById("overlay");
     el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
