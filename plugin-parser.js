3d0455df8c81981225256d931ef70a9b5712383a
poly <poly@darkdepths.net>
2015-09-17 19:31:29 +0400

    plugin list caching, closes #19

--- plugin-parser.js
+++ plugin-parser.js
@@ -1,33 +1,50 @@
 var path = require("path");
 var fs  = require("fs");
+var chokidar = require('chokidar'); // node fs.watch doesn't work recursively
+
+var plugins = [];
+var pluginModifications = [];
+
+chokidar.watch(path.resolve(__dirname + path.sep + 'plugins'),
+            {ignored: /[\/\\]\./}).on('all', function(event, path) {
+      pluginModifications.push(event);
+});
 
 exports.loadPlugins = function(callback) {
-    var plugins = [];
     var pluginFolder = path.resolve(__dirname + path.sep + 'plugins');
-    
-    fs.readdir(pluginFolder, function(err, files) {
-        if (err) {
-            console.log(err);
-            return;
-        }
 
-        files.forEach(function(f, index, array) {
-            var fullFile = pluginFolder + path.sep + f;
-            fs.stat(fullFile, function(err, stats){
-                if (stats.isDirectory()) {
-                    var mainFile = path.resolve(fullFile) + path.sep + 'main.json';
-                    fs.access(mainFile, fs.R_OK, function(err) {
-                        if (!err) {
-                            plugins.push(loadJson(mainFile));
-                        }
-                        if (plugins.length > 0) {
-                            callback(plugins);
-                        }
-                    });
-                }
+    if (plugins.length == 0 || pluginModifications.length != 0) {
+        // cache miss
+        plugins = []; // reset in case changes made to filesystem
+        pluginModifications = [];
+
+        fs.readdir(pluginFolder, function(err, files) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            files.forEach(function(f, index, array) {
+                var fullFile = pluginFolder + path.sep + f;
+                fs.stat(fullFile, function(err, stats){
+                    if (stats.isDirectory()) {
+                        var mainFile = path.resolve(fullFile) + path.sep + 'main.json';
+                        fs.access(mainFile, fs.R_OK, function(err) {
+                            if (!err) {
+                                plugins.push(loadJson(mainFile));
+                            }
+                            if (plugins.length > 0) {
+                                callback(plugins);
+                            }
+                        });
+                    }
+                });
             });
         });
-    });
+    } else {
+        // cache hit
+        callback(plugins);
+    }
 };
 
 var loadJson = function(file) {
