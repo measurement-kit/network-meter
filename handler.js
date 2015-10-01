732215c05d229efac1e3910bd2e59cee2b387162
poly <poly@darkdepths.net>
2015-10-01 16:21:22 +0400

    added skeleton for test handler

---
+++ handler.js
@@ -0,0 +1,17 @@
+/* handler.js
+ * Manages tests running in parallel
+ */
+var startHandler = function() {
+}
+
+var getRunningTests = function() {
+}
+
+var getFinishedTests = function() {
+}
+
+var startTest = function() {
+}
+
+var killTest = function() {
+}
