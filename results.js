bdf6b87afd62c24ebf79338b7e23605eb1edfb16
poly <poly@darkdepths.net>
2015-10-02 18:22:25 +0400

    display finished tests

---
+++ results.js
@@ -0,0 +1,9 @@
+/* Handles the report output from a plugin */
+
+/* collectOutput(plugin, callback):
+ * takes a plugin struct and a callback function.
+ * calls the output of the plugin as a string with the
+ * callback function
+ */
+exports.collectOutput = function(plugin, callback) {
+}
