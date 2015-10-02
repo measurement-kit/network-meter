a1d7688c00ffa4c5dc6b19c339f7c3d02f27a1e1
poly <poly@darkdepths.net>
2015-10-02 14:59:47 +0400

    skeleton class for main.js

---
+++ plugins/ooni/main.js
@@ -0,0 +1,41 @@
+/* Example configuration file for OONI plugin
+ */
+
+/* info:
+ * general metadata about the plugin
+ */
+var info = {
+        "name": "ooni",
+        "short": "Open Observatory of Network Interference",
+        "description": "A free software, global observation network for detecting censorship, surveillance and traffic manipulation on the internet",
+        "version": "1.0.0",
+        "exec": "ooniprobe",
+        "args": [],
+        "output": "STDOUT" | "-o",
+        "command": "-i example-deck/example.deck"
+};
+
+/* setup:
+ * array of commands to be run at time of installation
+ */
+var setup = ["", "", ""];
+
+/* regex(argName, val): 
+ * provided an arugment name and value, this function returns true
+ * if value passes the regex and false otherwise.
+ *
+ * stock regexes from TODO.js class are available for use.
+ */
+var regex = function(argName, val) {
+    return bool
+}
+
+/* parsing(rawData, elementHTML):
+ * function to visualize the data created by the test.
+ * rawData provides the output of the tool and 
+ * elementHTML provides an HTML element to draw inside.
+ *
+ * c3.js or d3.js libraries are available for use.
+ */
+var parsing = function(rawData, elementHTML) {
+}
