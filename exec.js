166ae8e3cc6e6ea7b1b29ded0f79eb0588b16857
poly <poly@darkdepths.net>
2015-11-17 09:25:07 +0400

    Js regexes
    - added dummy plugin for offline testing
    
    - break down complex function
    
    - removed HTML-based regex
    
    - toggle after run
    
    - fixes windows compat bug
    
    - updated plugins to new format
    
    - implemented JS based regex
    
    - renamed dummy plugin
    
    - handle invalid regexes

--- exec.js
+++ exec.js
@@ -2,6 +2,7 @@ var exec = require('child_process').execFile;
 var remote = require('remote'); 
 var dialog = remote.require('dialog'); 
 var run = require('./handler.js').startTest;
+var path = require("path");
 
 var fileName = null;
 var args     = null;
@@ -19,20 +20,26 @@ exports.bindPluginGrid = function(state){
 var bindRunButton = function(state) {
     document.getElementById("run-button").addEventListener("click", 
         function() { 
-            if (formValid())
-                collectArgs(state) 
+            if (formValid(state)) {
+                var arguments = collectArgs(state);
+                execute(arguments, state);
+                toggleOverlay();
+            }
         });
 };
 
-/* Reads arguments from the modal and generates a command
+/**
+ * Reads arguments from the modal and generates a command
  * to be run. Uses the template from plugins[x].command
- * and replaces flags as appropriate */
+ * and replaces flags as appropriate.
+ * Return list of supplied arguments.
+ */
 var collectArgs = function(state) {
+    var selectedIndex = state.run.get("selected");
+    
     var containers = document.getElementsByClassName("argument-container");
     var args = [];
-    var selectedIndex = state.run.get("selected");
-    var name = state.run.get("plugins[" + selectedIndex + "].name");
-
+    
     for(var i = 0; i < containers.length; i++) {
         // activation refers to a checkbox that signifies if the user has activated
         // the argument (only for optional arguments)
@@ -61,19 +68,12 @@ var collectArgs = function(state) {
             // for all other inputs, relevant input is always the last one
                 option = option[option.length - 1];
 
-            var optionName = option.name.replace("arg-", "");
-            optionName = optionName.replace("-activated", "");
+            var optionName = getRNameFromElement(option);
             var optionValue;
             
             // if the arg has a flag, prepend the flag
             var flagArray = state.run.get("plugins[" + selectedIndex + "].args");
-            var flagIndex;
-            for(var z = 0; z < flagArray.length; z++) {
-                if (flagArray[z].name == optionName) {
-                    flagIndex = z;
-                    break;
-                }
-            }
+            var flagIndex = getIndexFromArgName(optionName, state);
             var flag = flagArray[flagIndex].flag;
 
             if (option.value == "on" && option.type == "checkbox") {
@@ -84,9 +84,39 @@ var collectArgs = function(state) {
             args.push([optionName, optionValue]);
         }
     }
+    
+    return args
+};
 
+/** 
+ * Returns name Ractive can understand from
+ * DOM element
+ */
+var getRNameFromElement = function(element) {
+    var result = element.name.replace("arg-", "");
+    result = result.replace("-activated", "");
+    return result;
+}
+
+var getIndexFromArgName = function(elementName, state) {
+    var selectedIndex = state.run.get("selected");
+    var flagArray = state.run.get("plugins[" + selectedIndex + "].args");
+
+    for(var z = 0; z < flagArray.length; z++) {
+        if (flagArray[z].name == elementName)
+            return z
+    }
+}
+
+/**
+ * Takes arguments and reference to state, executes
+ * plugin with given values
+ */
+var execute = function(args, state) {
+    var selectedIndex = state.run.get("selected");
+   
+    var name = state.run.get("plugins[" + selectedIndex + "].name");
     var executable = state.run.get("plugins["+ selectedIndex + "].exec");
-
     var command = state.run.get("plugins[" + selectedIndex + "].command");
 
     for(var x = 0; x < args.length; x++) {
@@ -97,9 +127,8 @@ var collectArgs = function(state) {
     // gets rid of any variables that have not been specified
     command = executable + " " + command.replace(new RegExp(/\$[a-z][a-z0-9_]*\s/g), "");
 
-    // TODO breaks compatibility with windows, use fs.seperator instead
-    run(name, command, "./plugins/" + name + "/", state);
-};
+    run(name, command, path.resolve("plugins") + path.sep + name, state);
+}
 
 /* Toggles an overlay display with arguments for the user
  * to enter */
@@ -109,11 +138,46 @@ var toggleOverlay = function() {
     window.scrollTo(0,0);
 };
 
-var formValid = function() {
-    var argForm = document.getElementById("arg-form");
-    for(var i = 0; i < argForm.length; i++) {
-        if (!argForm[i].validity.valid)
-            return false;
+var formValid = function(state) {
+    var selectedIndex = state.run.get("selected");
+    var elements = document.getElementsByClassName("argument-container");
+
+    for(var i = 0; i < elements.length; i++) {
+        var textElement = elements[i].getElementsByTagName("input")[0];
+        if (textElement.type == "text") {
+            var textName = getRNameFromElement(textElement);
+            var textIndex = getIndexFromArgName(textName, state);
+            var regexType = state.run.get("plugins[" + selectedIndex +
+                            "].args[" + textIndex + "].regex");
+            var regex = placeholderToRegex(regexType);
+            if (regex.test(textElement.value) == false) 
+                return false;
+        }
     }
-    return true;
+
+    return true
+}
+
+var placeholderToRegex = function(input) {
+    switch (input) {
+        case "email":
+            return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+            break;
+        case "IP":
+            return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+            break;
+        case "domain":
+            return /^(?!:\/\/)([a-zA-Z0-9]+\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\.[a-zA-Z]{2,6}?$/i
+            break;
+        case "hostname":
+            return /(^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)|(^(?!:\/\/)([a-zA-Z0-9]+\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\.[a-zA-Z]{2,6}?$)/i
+            break;
+        case "URL":
+            return /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i
+            break;
+        default:
+            if (input != "")
+                console.warn("Invalid regex found");
+            return
+    }
 }
